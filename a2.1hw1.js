e13336b99b152627dd4e775c4bb7be3be0e8f50e
Mohit Kumar <mohitkumarxm@gmail.com>
2025-02-20 17:34:39 +0530

    operator 2.1 added

---
+++ a2.1hw1.js
@@ -0,0 +1,72 @@
+console.log("A2.1_HW_1");
+console.log("---- ---- ----");
+console.log("Exercise 1");
+
+let numOne = 8;
+let numTwo = 6;
+let numThree = 16;
+console.log("---- ---- ----");
+
+console.log("1.1");
+let sum = numOne + numTwo;
+
+console.log("Numbers: ", numOne, numTwo);
+console.log("Sum: ", sum);
+console.log("---- ---- ----");
+
+console.log("1.2");
+let difference = numOne - numTwo;
+console.log("Numbers: ", numOne, numTwo);
+console.log("Difference: ", difference);
+console.log("---- ---- ----");
+
+console.log("1.3");
+let product = numOne * numTwo;
+console.log("Numbers: ", numOne, numTwo);
+console.log("Product: ", product);
+console.log("---- ---- ----");
+
+console.log("1.4");
+let division = numOne / numTwo;
+console.log("Numbers: ", numOne, numTwo);
+console.log("Division: ", division);
+console.log("---- ---- ----");
+
+console.log("1.5");
+let remainder = numOne % numTwo;
+console.log("Numbers: ", numOne, numTwo);
+console.log("Remainder: ", remainder);
+console.log("---- ---- ----");
+
+console.log("Exercise 2");
+console.log("---- ---- ----");
+
+console.log("2.1");
+let sumAll = numOne + numTwo + numThree;
+console.log("Numbers: ", numOne, numTwo, numThree);
+console.log("Sum All: ", sumAll);
+console.log("---- ---- ----");
+
+console.log("2.2");
+let sumAndSubtract = numThree - (numOne + numTwo);
+console.log("Numbers: ", numOne, numTwo, numThree);
+console.log("Sum and Subtract: ", sumAndSubtract);
+console.log("---- ---- ----");
+
+console.log("2.3");
+let productAndAdd = numOne * numTwo + numThree;
+console.log("Numbers: ", numOne, numTwo, numThree);
+console.log("Product and Add: ", productAndAdd);
+console.log("---- ---- ----");
+
+console.log("2.4");
+let divideAndAdd = numThree / numOne + numTwo;
+console.log("Numbers: ", numOne, numTwo, numThree);
+console.log("Divide and Add: ", divideAndAdd);
+console.log("---- ---- ----");
+
+console.log("2.5");
+let average = (numOne + numTwo + numThree) / 3;
+console.log("Numbers: ", numOne, numTwo, numThree);
+console.log("Average: ", average);
+console.log("---- ---- ----");
